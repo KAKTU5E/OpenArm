468a0dcfe85f08ef98e67b4caea3ddbe4c31b744
KAKTU5E <supermaxpizz2006@gmail.com>
2026-04-05 18:13:39 -0400

    modified:   arm_config/setup.py 	modified:   arm_description/urdf/OpenArmVKillian.xacro

--- arm_config/setup.py
+++ arm_config/setup.py
@@ -1,15 +1,18 @@
-from setuptools import find_packages, setup
+from glob import glob
+from setuptools import setup
 
 package_name = 'arm_config'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
+    package_dir={package_name: 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
