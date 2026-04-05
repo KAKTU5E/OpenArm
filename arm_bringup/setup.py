34c662705887df095fb82e1ec5a1365c714cbca3
KAKTU5E <supermaxpizz2006@gmail.com>
2026-04-05 18:04:42 -0400

    deleted:    arm_bringup/arm_bringup/arm.launch.py 	modified:   arm_bringup/setup.py 	modified:   arm_config/package.xml

--- arm_bringup/setup.py
+++ arm_bringup/setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+import os
+from setuptools import find_packages, setup
+from glob import glob
+
 
 package_name = 'arm_bringup'
 
@@ -10,6 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
